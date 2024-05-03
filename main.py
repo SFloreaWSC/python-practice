fbf09e44c773f75355d5f96f1d7d0c6ce4cb949c
SFloreaWSC <floreas@wsc.school.nz>
2024-05-03 12:45:23 +1200

    s

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+#var setting
+inst = 0
+temporary = 0
+
 #ask user's name
-print ("Hello, you")
+print ("Hello, you.")
 name = input ("What is your name?") 
 
 #greet user using name
@@ -7,14 +11,27 @@ print (f"Nice to meet you, {name}")
 print ("I have a question for you.")
 
 #ask if they feel like they are being watched
-answer1 = input ("Do you feel like you are being watched? A) Yes. B) No.")
-
-#tell correct answer
-if answer1 == "A" or answer1 == "a":
+a1 = input ("Do you feel like you are being watched? A) Yes B) No")
+if a1 == "A" or a1 == "a":
     print ("Congratulations, That was correct!")
 else:
     print ("I'm sorry, the correct answer was: Yes.")
 
+#INSERT CODE LAST QUESTION
+if temporary == 3:
+    print (f"Thank you for this valuble data, {name}.")
+    print ("Goodbye.")
+
+#add up score
 #say goodbye (use user name)
-print (f"Thank you for this valuble data, {name}.")
-print ("Goodbye.")
+
+
+#refuse to answer override
+if a1 == "" or a1 == " ":
+    inst = inst + 1
+    print ("Please answer the question.")
+    a1 = "dhaifjhvnkdfghnmvkdiuhfbnmgkfreifgubhfenjdifvobifjdebdfjiofdeidhfbgjfdiefjbnfdjij"
+    a1 = input ("Do you feel like you are being watched? A) Yes B) No")
+if a1 == "" or a1 == " " and inst == 1:
+    print ("We do not have time for insolence.")
+    print ("Goodbye.")
