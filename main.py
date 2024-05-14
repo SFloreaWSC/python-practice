0eb28441cb7ba16471614a1f61010d1df4cea885
SFloreaWSC <floreas@wsc.school.nz>
2024-05-14 21:50:16 +1200

    ert

--- main.py
+++ main.py
@@ -1,213 +1,238 @@
 #setting
-inst = 0
-temporary = 0
-points = 0
-QUESTION_FORMAT = "A){}"
-#ask user's name
-print ("Hello, you.")
-name = input ("What is your name?")     
+replay = "yes"
+while replay == "yes":
+    #setting
+    inst = 0
+    temporary = 0
+    points = 0
 
-#greet user using name
-print (f"Nice to meet you, {name}.")
-print ("The questionnaire is ready for your participation.")
+    #ask user's name
+    print ("Hello, you.")
+    name = input ("What is your name?")     
 
-#question 0
-ans = input ("When did you last do something bad? \nA) Very recently \nB) Recently\nC) Not recently\nD) Never\n".lower())
-if ans == "" or ans == " ":
-    inst = inst + 1
-    print ("Please answer the question.")
-    ans = input ("When did you last do something bad? \nA) Very recently \nB) Recently\nC) Not recently\nD) Never\n")
+    #greet user using name
+    print (f"Nice to meet you, {name}.")
+    print ("The questionnaire is ready for your participation.")
+
+    #question 0
+    ans = input ("When did you last do something bad? \nA) Very recently \nB) Recently\nC) Not recently\nD) Never\n").lower()
     if ans == "" or ans == " ":
         inst = inst + 1
         print ("Please answer the question.")
-        ans = input ("When did you last do something bad? \nA) Very recently \nB) Recently\nC) Not recently\nD) Never\n")
+        ans = input ("When did you last do something bad? \nA) Very recently \nB) Recently\nC) Not recently\nD) Never\n").lower()
         if ans == "" or ans == " ":
             inst = inst + 1
-if ans == "A" or ans == "a" or ans == "Very recently" or ans == "very recently":
-    points = points + 1
-    ans = "z"
-else:
-    ans = "z"   
+            print ("Please answer the question.")
+            ans = input ("When did you last do something bad? \nA) Very recently \nB) Recently\nC) Not recently\nD) Never\n").lower()
+            if ans == "" or ans == " ":
+                inst = inst + 1
+    if ans == "a" or ans == "very recently":
+        points = points + 1
+        print(points)
+        ans = "z"
+    else:
+        ans = "z"   
 
-#question 1
-ans = input ("Do you think that you are a good person at heart? \nA) Yes \nB) Who can say\nC) No\n")
-if ans == "" or ans == " ":
-    inst = inst + 1
-    print ("Please answer the question.")
-    ans = input ("Do you think that you are a good person at heart? \nA) Yes \nB) Who can say\nC) No\n")
+    #question 1
+    ans = input ("Do you think that you are a good person at heart? \nA) Yes \nB) No\nC) Who can say\n").lower()
     if ans == "" or ans == " ":
         inst = inst + 1
         print ("Please answer the question.")
-        ans = input ("Do you think that you are a good person at heart? \nA) Yes \nB) Who can say\nC) No\n")
+        ans = input ("Do you think that you are a good person at heart? \nA) Yes \nB) Who can say\nC) No\n").lower()
         if ans == "" or ans == " ":
             inst = inst + 1
-if ans == "B" or ans == "b" or ans == "Who can say" or ans == "who can say":
-    points = points + 1
-    ans = "z"
-else:
-    ans = "z"
+            print ("Please answer the question.")
+            ans = input ("Do you think that you are a good person at heart? \nA) Yes \nB) Who can say\nC) No\n").lower()
+            if ans == "" or ans == " ":
+                inst = inst + 1
+    if ans == "c" or ans == "who can say":
+        points = points + 1
+        ans = "z"
+    else:
+        ans = "z"
 
-#question 2
-ans = input ("Would you steal 100,000 of untraceable cash from a man on his deathbed? \nA)Yes \nB)No \nC)Wouldn't you\n")
-if ans == "" or ans == " ":
-    inst = inst + 1
-    print ("Please answer the question.")
-    ans = input ("Would you steal 100,000 of untraceable cash from a man on his deathbed? \nA)Yes \nB)No \nC)Wouldn't you\n")
+    #question 2
+    ans = input ("Would you steal 100,000 of untraceable cash from a man on his deathbed? \nA)Yes \nB)No \nC)Wouldn't you\n").lower()
     if ans == "" or ans == " ":
         inst = inst + 1
         print ("Please answer the question.")
-        ans = input ("Would you steal 100,000 of untraceable cash from a man on his deathbed? \nA)Yes \nB)No \nC)Wouldn't you\n")
+        ans = input ("Would you steal 100,000 of untraceable cash from a man on his deathbed? \nA)Yes \nB)No \nC)Wouldn't you\n").lower()
         if ans == "" or ans == " ":
             inst = inst + 1
-if ans == "C" or ans == "c" or ans == "Wouldn't you" or ans == "wouldn't you":
-    points = points + 1
-    ans = "z"
-else:
-    ans = "z"
+            print ("Please answer the question.")
+            ans = input ("Would you steal 100,000 of untraceable cash from a man on his deathbed? \nA)Yes \nB)No \nC)Wouldn't you\n").lower()
+            if ans == "" or ans == " ":
+                inst = inst + 1
+    if ans == "c" or ans == "wouldn't you":
+        points = points + 1
+        ans = "z"
+    else:
+        ans = "z"
 
-#question 3
-ans = input ("Do you feel like you are being watched? \nA) Yes \nB) No \nC) I am being watched\n")
-if ans == "" or ans == " ":
-    inst = inst + 1
-    print ("Please answer the question.")
-    ans = input ("Do you feel like you are being watched? \nA) Yes \nB) No\nC) I am being watched\n")
+    #question 3
+    ans = input ("Do you feel like you are being watched? \nA) Yes \nB) No \nC) I am being watched\n").lower()
     if ans == "" or ans == " ":
         inst = inst + 1
         print ("Please answer the question.")
-        ans = input ("Do you feel like you are being watched? \nA) Yes \nB) No\nC) I am being watched\n")
+        ans = input ("Do you feel like you are being watched? \nA) Yes \nB) No\nC) I am being watched\n").lower()
         if ans == "" or ans == " ":
             inst = inst + 1
-if ans == "C" or ans == "c" or ans == "I am being watched" or ans == "i am being watched":
-    points = points + 1
-    ans = "z"
-else:
-    ans = "z"
+            print ("Please answer the question.")
+            ans = input ("Do you feel like you are being watched? \nA) Yes \nB) No\nC) I am being watched\n").lower()
+            if ans == "" or ans == " ":
+                inst = inst + 1
+    if ans == "c" or ans == "i am being watched":
+        points = points + 1
+        ans = "z"
+    else:
+        ans = "z"
 
-#question 4
-ans = input ("How often do you feel like life is meaningless? \nA) Frequently \nB) Occasionally \nC) Rarely \nD) Never\n")
-if ans == "" or ans == " ":
-    inst = inst + 1
-    print ("Please answer the question.")
-    ans = input ("How often do you feel like life is meaningless? \nA) Frequently \nB) Occasionally \nC) Rarely \nD) Never\n")
+    #question 4
+    ans = input ("How often do you feel like life is meaningless? \nA) Frequently \nB) Occasionally \nC) Rarely \nD) Never\n").lower()
     if ans == "" or ans == " ":
         inst = inst + 1
         print ("Please answer the question.")
-        ans = input ("How often do you feel like life is meaningless? \nA) Frequently \nB) Occasionally \nC) Rarely \nD) Never\n")
+        ans = input ("How often do you feel like life is meaningless? \nA) Frequently \nB) Occasionally \nC) Rarely \nD) Never\n").lower()
         if ans == "" or ans == " ":
             inst = inst + 1
-if ans == "B" or ans == "b" or ans == "Occasionally" or ans == "occasionally":
-    points = points + 1
-    ans = "z"
-else:
-    ans = "z"
+            print ("Please answer the question.")
+            ans = input ("How often do you feel like life is meaningless? \nA) Frequently \nB) Occasionally \nC) Rarely \nD) Never\n").lower()
+            if ans == "" or ans == " ":
+                inst = inst + 1
+    if ans == "b" or ans == "occasionally":
+        points = points + 1
+        ans = "z"
+    else:
+        ans = "z"
 
-#question 5
-ans = input ("If you went missing, is there anyone who would miss you? \nA) Yes \nB) No\n")
-if ans == "" or ans == " ":
-    inst = inst + 1
-    print ("Please answer the question.")
-    ans = input ("If you went missing, is there anyone who would miss you? \nA) Yes \nB) No\n")
+    #question 5
+    ans = input ("If you went missing, is there anyone who would miss you? \nA) There isn't \nB) There is\n").lower()
     if ans == "" or ans == " ":
         inst = inst + 1
         print ("Please answer the question.")
-        ans = input ("If you went missing, is there anyone who would miss you? \nA) Yes \nB) No\n")
+        ans = input ("If you went missing, is there anyone who would miss you? \nA) Yes \nB) No\n").lower()
         if ans == "" or ans == " ":
             inst = inst + 1
-if ans == "B" or ans == "b" or ans == "No" or ans == "no":
-    points = points + 1
-    ans = "z"
-else:
-    ans = "z"
+            print ("Please answer the question.")
+            ans = input ("If you went missing, is there anyone who would miss you? \nA) Yes \nB) No\n").lower()
+            if ans == "" or ans == " ":
+                inst = inst + 1
+    if ans == "a" or ans == "there isn't":
+        points = points + 1
+        ans = "z"
+    else:
+        ans = "z"
 
-#question 6
-ans = input ("Do you regret the things you have done which you believe served the ‘greater good’? \nA) I do\nB) I don't\nC) I can't\nD) I won't\n")
-if ans == "" or ans == " ":
-    inst = inst + 1
-    print ("Please answer the question.")
-    ans = input ("Do you regret the things you have done which you believe served the ‘greater good’? \nA) I do\nB) I don't\nC) I can't\nD) I won't\n")
+    #question 6
+    ans = input ("Do you regret the things you have done which you believe served the ‘greater good’? \nA) I do\nB) I don't\nC) I can't\nD) I won't\n").lower()
     if ans == "" or ans == " ":
         inst = inst + 1
         print ("Please answer the question.")
-        ans = input ("Do you regret the things you have done which you believe served the ‘greater good’? \nA) I do\nB) I don't\nC) I can't\nD) I won't\n")
+        ans = input ("Do you regret the things you have done which you believe served the ‘greater good’? \nA) I do\nB) I don't\nC) I can't\nD) I won't\n").lower()
         if ans == "" or ans == " ":
             inst = inst + 1
-if ans == "C" or ans == "c" or ans == "I can't" or ans == "i can't":
-    points = points + 1
-    ans = "z"
-else:
-    ans = "z"
+            print ("Please answer the question.")
+            ans = input ("Do you regret the things you have done which you believe served the ‘greater good’? \nA) I do\nB) I don't\nC) I can't\nD) I won't\n").lower()
+            if ans == "" or ans == " ":
+                inst = inst + 1
+    if ans == "c" or ans == "i can't":
+        points = points + 1
+        ans = "z"
+    else:
+        ans = "z"
 
-#question 7
-ans = input ("Do you regret the things you have done which did not serve the ‘greater good’? \nA) I do\nB) I don't\nC) I can't\nD) I won't\n")
-if ans == "" or ans == " ":
-    inst = inst + 1
-    print ("Please answer the question.")
-    ans = input ("Do you regret the things you have done which you believe served the ‘greater good’? \nA) I do\nB) I don't\nC) I can't\nD) I won't\n")
+    #question 7
+    ans = input ("Do you regret the things you have done which did not serve the ‘greater good’? \nA) I do\nB) I don't\nC) I can't\nD) I won't\n").lower()
     if ans == "" or ans == " ":
         inst = inst + 1
         print ("Please answer the question.")
-        ans = input ("Do you regret the things you have done which you believe served the ‘greater good’? \nA) I do\nB) I don't\nC) I can't\nD) I won't\n")
+        ans = input ("Do you regret the things you have done which you believe served the ‘greater good’? \nA) I do\nB) I don't\nC) I can't\nD) I won't\n").lower()
         if ans == "" or ans == " ":
             inst = inst + 1
-if ans == "D" or ans == "d" or ans == "I won't" or ans == "i won't":
-    points = points + 1
-    ans = "z"
-else:
-    ans = "z"
+            print ("Please answer the question.")
+            ans = input ("Do you regret the things you have done which you believe served the ‘greater good’? \nA) I do\nB) I don't\nC) I can't\nD) I won't\n").lower()
+            if ans == "" or ans == " ":
+                inst = inst + 1
+    if ans == "d" or ans == "i won't":
+        points = points + 1
+        ans = "z"
+    else:
+        ans = "z"
 
-#question 8
-ans = input ("Have you declared your participation in this questionnaire to anyone? \nA) I have\nB) I haven't\n")
-if ans == "" or ans == " ":
-    inst = inst + 1
-    print ("Please answer the question.")
-    ans = input ("Have you declared your participation in this questionnaire to anyone? \nA) I have\nB) I haven't\n")
+    #question 8
+    ans = input ("Have you declared your participation in this questionnaire to anyone? \nA) I have\nB) I haven't\n").lower()
     if ans == "" or ans == " ":
         inst = inst + 1
         print ("Please answer the question.")
-        ans = input ("Have you declared your participation in this questionnaire to anyone? \nA) I have\nB) I haven't\n")
+        ans = input ("Have you declared your participation in this questionnaire to anyone? \nA) I have\nB) I haven't\n").lower()
         if ans == "" or ans == " ":
             inst = inst + 1
-if ans == "B" or ans == "b" or ans == "I haven't" or ans == "i haven't":
-    points = points + 1
-    ans = "z"
-else:
-    ans = "z"
+            print ("Please answer the question.")
+            ans = input ("Have you declared your participation in this questionnaire to anyone? \nA) I have\nB) I haven't\n").lower()
+            if ans == "" or ans == " ":
+                inst = inst + 1
+    if ans == "b" or ans == "i haven't":
+        points = points + 1
+        ans = "z"
+    else:
+        ans = "z"
 
-#question 9
-ans = input ("Would anyone investigate if you suddenly were declared as missing? \nA) Yes\nB) No\n")
-if ans == "" or ans == " ":
-    inst = inst + 1
-    print ("Please answer the question.")
-    ans = input ("Would anyone investigate if you suddenly were declared as missing? \nA) Yes\nB) No\n")
+    #question 9
+    ans = input ("Would anyone investigate if you suddenly were declared as missing? \nA) Yes\nB) No\n").lower()
     if ans == "" or ans == " ":
         inst = inst + 1
         print ("Please answer the question.")
-        ans = input ("Would anyone investigate if you suddenly were declared as missing? \nA) Yes\nB) No\n")
+        ans = input ("Would anyone investigate if you suddenly were declared as missing? \nA) Yes\nB) No\n").lower()
         if ans == "" or ans == " ":
             inst = inst + 1
-if ans == "B" or ans == "b" or ans == "No" or ans == "no":
-    points = points + 1
-    ans = "z"
-else:
-    ans = "z"
+            print ("Please answer the question.")
+            ans = input ("Would anyone investigate if you suddenly were declared as missing? \nA) Yes\nB) No\n").lower()
+            if ans == "" or ans == " ":
+                inst = inst + 1
+    if ans == "b" or ans == "no":
+        points = points + 1
+        ans = "z"
+    else:
+        ans = "z"
 
-if inst > 2:
-    print("We do not have time for insolence.")
-    print(f"Goodbye, {name}.")
-#END OF QUOESAIRIAIREEREE
-if inst == 0:
-    print("Thank you for answering all of our questions.")
-    print(points)
-elif inst == 1 or inst == 2:
-    print("Thank you for, mostly, answering all the questions.")
-#override
+    #end of questionnaire
+    #override
+    while replay == "yes" and inst > 2:
+        print("We do not have time for insolence.")
+        print(f"Goodbye, {name}.")
+        replay == "no"
+    if inst == 0:
+        print("Thank you for answering all of our questions.")
+    elif inst == 1 or inst == 2:
+        print("Thank you for, for the most part, answering all our questions.")
+    #points
+    if points < 4:
+        print("This data will be useful moving forward in out endeavors,")
+        print(f"Goodbye, {name}.")
+        re = 1
+    if points >= 4 and points <= 9:
+        print("Nicely done, you have been placed on a waiting list.")
+        print("If you are picked, we will contact you, do not try and contact us.")
+        print(f"Thank you, {name}")
+        re = 1
+    if points == 10:
+        print("Well done, you are in the 99th percentile of participants.")
+        print("Members of our corporation have been dispatched, please refrain from exiting the premises.")
+        print(f"Thank you, {name}")
+        re = 1
+    if points > 10:
+        print("Please do not go anywhere, BAASA associates will be arriving soon.")
+        print("Do not be alarmed. Do not leave. Do not resist.")
+        print("ESA")
+    
+    while re == 1:
+        replaie = input("If you would like to sumbit another questionnaire, please state so now. \n1) Yes\n2) No\n").lower()
+        if replaie == "1" or replaie == "yes":
+            replay = "yes"
+            re = 0
+        elif replaie == "2" or replaie == "no":
+            print("Goodbye.")
+            replay = "no"
+            re = 0
 
-#points
-    print("a")
-if points >= 4 and points <= 9:
-    print("b")
-if points == 10:
-    print("c")
-if points > 10:
-    print("???")
 
